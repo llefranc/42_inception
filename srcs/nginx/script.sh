7198f91fb5e358d3417e54dc55ac416f68cf31fc
lucas lefrancq <lucas.lefrancq@hotmail.fr>
2021-06-28 17:17:10 +0200

    makefile added, docker compose created, nginx working

--- srcs/nginx/script.sh
+++ srcs/nginx/script.sh
@@ -5,13 +5,13 @@ openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx
 		-keyout /etc/nginx/tls/nginx.key -subj '/CN=localhost'
 
 # Then starting nginx
-nginx
+nginx -g "daemon off;"
 
 # Looping while nginx process exist. If nginx is killed, the script exit and so the
 # container will exit too as this script was the entrypoint.
-STATUS=1
-while [ $STATUS -ne 0 ]; do
-    echo $STATUS
-    sleep 1
-    STATUS=$(/usr/bin/pgrep nginx | wc -l)
-done
+# STATUS=1
+# while [ $STATUS -ne 0 ]; do
+#     echo $STATUS
+#     sleep 1
+#     STATUS=$(/usr/bin/pgrep nginx | wc -l)
+# done
