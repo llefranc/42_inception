9e39f6228f8eaf76110ab90921c3a40889107c34
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2021-06-28 15:04:01 +0200

    nginx container working

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 42_inception
+
+[How to config a HTTPS server][2]
+[Create a TLS certificate][1]
+[Looping in a shell script until until a process die][3]
+
+[1]:https://www.linode.com/docs/guides/create-a-self-signed-tls-certificate/
+[2]:http://nginx.org/en/docs/http/configuring_https_servers.html
+[3]:https://stackoverflow.com/questions/38666191/while-loop-in-bash-that-uses-pgrep-to-check-if-service-exists
---
+++ nginx/script.sh
@@ -0,0 +1,17 @@
+# !/bin/sh
+
+# Creating TLS certificate
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/tls/nginx.crt \
+		-keyout /etc/nginx/tls/nginx.key -subj '/CN=localhost'
+
+# Then starting nginx
+nginx
+
+# Looping while nginx process exist. If nginx is killed, the script exit and so the
+# container will exit too as this script was the entrypoint.
+STATUS=1
+while [ $STATUS -ne 0 ]; do
+    echo $STATUS
+    sleep 1
+    STATUS=$(/usr/bin/pgrep nginx | wc -l)
+done
